ba174e1d642de1844dbdd567b996ba377f29a0cd
Dominick Cleary <dominickcleary@gmail.com>
2018-11-28 16:14:03 +0000

    updated to include chart of commit days

--- social_graph.py
+++ social_graph.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from random import randint
+import datetime
 from github import Github   # PyGithub: https://github.com/PyGithub/PyGithub
 
 # Returns a dictionary containing the language used and amount of repos that use them
@@ -29,9 +29,19 @@ def getAvgLanguageDetails(dictList):
 
     return newDict
     
+# Creates one pie chart
+def pieChart(values, labels, title, windowName):
+    plt.figure(num=windowName)
+    plt.pie(values, labels=labels, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.title(title)
+
+    plt.show()
+
 # Creates two pie charts side by side
-def pieChart(values1, labels1, values2, labels2, title1, title2):
-    
+def pieChartTwo(values1, labels1, values2, labels2, title1, title2, windowName):
+    plt.figure(num=windowName)
     plt.subplot(1, 2, 1)
     plt.pie(values1, labels=labels1, autopct='%1.1f%%', startangle=90)
     plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
@@ -47,7 +57,21 @@ def pieChart(values1, labels1, values2, labels2, title1, title2):
     plt.show()
 
 # Creates a bar chart
-def barChartH(x, y, xlabel, ylabel, title):
+def barChart(x, y, xlabel, ylabel, title, windowName):
+    plt.figure(num=windowName)
+    y_pos = np.arange(len(y))
+
+    plt.bar(y_pos, list(x), align='center')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.yticks(y_pos, y)
+    plt.title(title)
+    
+    plt.show()
+    
+# Creates a horizontal bar chart
+def barChartH(x, y, xlabel, ylabel, title, windowName):
+    plt.figure(num=windowName)
     y_pos = np.arange(len(y))
 
     plt.barh(y_pos, list(x), align='center')
@@ -67,7 +91,7 @@ def compareLanguageUse(user):
 
     avgDict = getAvgLanguageDetails(dictList)
     userDict = getLanguageDetails(user)
-    pieChart(userDict.values(), userDict, avgDict.values(), avgDict, user.name + "'s Language Use", "Followers Average Language Use")
+    pieChartTwo(userDict.values(), userDict, avgDict.values(), avgDict, user.name + "'s Language Use", "Followers Average Language Use", "Comparison of languages used")
 
 # Compares the amount of commits between all the users repos
 def compareCommitsPerRepo(user):
@@ -77,10 +101,18 @@ def compareCommitsPerRepo(user):
         for commit in repo.get_commits():
             count += 1
         repoInfo[repo.name] = count
-    barChartH(repoInfo.values(), repoInfo, "Commits", "Repos", "Amount of Commits per Repo")
+    barChartH(repoInfo.values(), repoInfo, "Commits", "Repos", "Amount of Commits per Repo", "Amount of Commits per Repo")
 
+def compareCommitDays(user):
+    dayList = [0] * 7
+    for repo in user.get_repos():
+        for commit in repo.get_commits():
+            weekday = commit.commit.author.date.weekday()
+            dayList[weekday] = dayList[weekday] + 1
+    pieChart(dayList, ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], "Distribution of Days that " + user.name + " Commits on", "Comparison of Commit Days")
 
-    
+def compateCommitTimes(user):
+    timeList = [0] * 
 # print("Hi, Welcome to Yet Another GitHub Analytics Program!\n\nHow would you like to sign in?\n\n1 = Username & Password\n2 = Token\n")
 
 # Login prompts
@@ -101,6 +133,7 @@ g = Github("860f00f494ad2a882e4a9ed1a7f55f9598bd9add")
 
 # Login
 user = g.get_user()
-# compareLanguageUse(user)
+compareLanguageUse(user)
 compareCommitsPerRepo(user)
+compareCommitDays(user)
 
